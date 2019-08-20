59b531b9b8372d0b5903786fd03b7a4e254b3533
Kua <35482811+KuaLai@users.noreply.github.com>
2019-08-21 02:33:00 +0800

    Feat: 優化寫法

--- exercise01/js/magic.js
+++ exercise01/js/magic.js
@@ -1,79 +1,93 @@
 // define global variable
-let obj, value;
-let canvas = document.getElementById("canvas"),
-    spirit, startX, startY;
-let demo = document.querySelector(".demo"),
+
+const canvas = document.getElementById("canvas");
+const demo = document.querySelector(".demo"),
     endcut = document.querySelector(".endCut"),
     tips = document.querySelector(".tips__mask"),
     arrow = document.querySelector(".arrow"),
     bg = document.querySelectorAll(".star__bg .item");
-let point = document.querySelectorAll(".star__item .item");
-let magicarr = [];
-let end;
-let name;
-let statusList = [0, 0, 0, 0, 0];
-let pointX = 0;
-let pointY = 0;
+const point = document.querySelectorAll(".star__item .item");
 
-function objStyle(obj, value) { obj.style.display = value; };
+let obj, value, end, name;
+let magicarray = [];
+let endArray = [];
+let checkarray = [];
+let statusList = [0, 0, 0, 0, 0];
+
+const touchRange = 40;
+
+function elementDisplayStatus(element,value){
+    if(element.length > 1){
+        for (let index = 0; index < element.length; index++) {
+            element[index].style.display = value;   
+        }
+    } else {
+        element.style.display = value; 
+    }
+    
+}
 
 function getPosition(element) {
     let x = 0;
     let y = 0;
-    while (element) {
-        x += element.offsetLeft - element.scrollLeft + element.clientLeft;
-        y += element.offsetTop - element.scrollLeft + element.clientTop;
-        element = element.offsetParent;
-    }
+
+    x += element.offsetLeft - element.scrollLeft + element.clientLeft;
+    y += element.offsetTop - element.scrollLeft + element.clientTop;
+    element = element.offsetParent;
     return { x: x, y: y };
 };
 
-function setPoint(element,index){
+function setPoint(element) {
+    let pointX = 0;
+    let pointY = 0;
+
     pointX = getPosition(element).x;
     pointY = getPosition(element).y;
-    return { pointX: pointX, pointY: pointY , index: index };
+    return { x: pointX, y: pointY };
 };
 
-function callbackSwitch(elementX,elementY,startX,startY,index){
-    let num = index;
-    let allLength = statusList.length;
+function isTouched(touchPosition, elementPosition) {
+    return touchPosition.pageX > (elementPosition.x - touchRange) && touchPosition.pageX < (elementPosition.x + touchRange) && touchPosition.pageY > (elementPosition.y - touchRange) && touchPosition.pageY < (elementPosition.y + touchRange);
+};
 
-    // isTouched()
-    
-    if (startX > (elementX - 40) && startX < (elementX + 40) && startY < (elementY + 40) && startY > (elementY - 40)) {
+function setFirstPointToOriginalStatus(num) {
+    statusList[0] = 0;
+    point[0].classList.remove("touch");
+    statusList[num] = num + 1;
+    name = "end";
+    num = num + 1;
+    end = true;
+    return (checkOptin(name));
+};
+
+function callbackSwitch(touchPosition, elementPosition, index) {
+    let num = index;
+    const allLength = statusList.length;
+
+    if (isTouched(touchPosition, elementPosition)) {
+
         if (statusList[num] != (num + 1)) {
-            statusList[num] = num + 1;   
+            statusList[num] = num + 1;
             name = 'open' + num;
             num = num + 1;
-            return(checkOptin(name));
-        } 
-        if ( statusList[num] == allLength ){
-
-
-            // setFirstPointToOriginalStatus
-            statusList[0] = 0;  
-            point[0].classList.remove("touch");
-            statusList[num] = num + 1; 
-            name = "end";
-            num = num + 1;
-            end = true;
-            return(checkOptin(name));
+            return (checkOptin(name));
         }
-    } 
+        if (statusList[num] == allLength) {
+            setFirstPointToOriginalStatus(num);
+        }
+    }
 };
 
 function checkOptin(name) {
     for (let i = 0; i < statusList.length; i++) {
         if (name == ("open" + i)) {
             point[i].classList.add("touch");
-            magicarr.push(i);
-            checkarr[0] = (magicarr[0]);
-
+            magicarray.push(i);
+            checkarray[0] = (magicarray[0]);
             if (end == true) {
-                checkArray("end");
-
+                check("end");
             } else {
-                checkArray(i);
+                check(i);
             }
         }
     }
@@ -83,46 +97,32 @@ function checkOptin(name) {
 function touchstart(event) {
     event.preventDefault();
     if (!event.touches.length) return;
-    
-    objStyle(arrow, "none");
+    elementDisplayStatus(arrow,"none");
 
 };
 
 function touchmove(event) {
-    event.preventDefault();
     if (!event.touches.length) return;
     let touch = event.touches[0];
-    startX = touch.pageX;
-    startY = touch.pageY;
-    let initialPoint ;
     for (let i = 0; i < statusList.length; i++) {
-        initialPoint = setPoint(point[i],i);
-        callbackSwitch(initialPoint.pointX,initialPoint.pointY,startX,startY,i);
+        callbackSwitch(touch, setPoint(point[i]), i);
         if (i > statusList.length) {
             return;
         }
     };
-    
+
 }
+
 function touchend() {
     // checkTouch();
 }
-// add touch start listener
-
-canvas.addEventListener("touchstart", touchstart, false);
-canvas.addEventListener("touchend", touchend, false);
-
-canvas.addEventListener("touchmove", touchmove,false);
 
 // replay
 function replay() {
-    objStyle(tips, "none");
-    objStyle(bg[1], "none");
-    objStyle(bg[0], "block");
-    objStyle(demo, "block");
-    objStyle(endcut, "none");
-    objStyle(arrow, "block");
-    magicarr = []; checkarr = [];
+    elementDisplayStatus([arrow,bg[0],demo],"block");
+    elementDisplayStatus([tips,bg[1],endcut],"none");
+    magicarray = []; 
+    checkarray = [];
     end = false;
     for (let i = 0; i < point.length; i++) {
         point[i].classList.remove("touch");
@@ -131,48 +131,59 @@ function replay() {
 };
 
 // check array
-let endArray = [];
-let checkarr = [];
-function checkArray(item){
-    console.log(checkarr, magicarr);
-    let line = _.isEqual(checkarr, magicarr);
-    if (line === false) {
-        objStyle(tips, "block");
+
+function addOrdertoList(i) {
+    if (checkarray.length < statusList.length) {
+        checkarray.push(i + 1);
     }
-   if (magicarr[0] == 0){
-        for (let i = 0; i < statusList.length ; i++) {
+    else {
+        checkarray.push(checkarray[0]);
+    }
+}
 
-            if ( item == i  ){
-                if(checkarr.length < statusList.length )
-                    checkarr.push( i + 1);
+function check(item) {
+    console.log(checkarray, magicarray);
+    let line = _.isEqual(checkarray, magicarray);
+    if (line === false) {
+        elementDisplayStatus(tips,"block");
+    }
 
-                else
-                    checkarr.push( checkarr[0] );  
+    if (magicarray[0] == 0) {
+        for (let i = 0; i < statusList.length; i++) {
+
+            if (item == i) {
+                addOrdertoList(i);
             }
-            if ( item == "end"){               
-                endArray = checkarr;
-                checkTouch();
-            }   
 
+            if (item == "end") {
+                endArray = checkarray;
+                checkTouch();
+            }
         }
     }
 
-    else
-        objStyle(tips, "block");
-    
-    
+    else {
+        elementDisplayStatus(tips,"block");
+    }
 };
 
-function checkTouch(){
-    let finalarray = _.isEqual(endArray, magicarr);
-    
+function checkTouch() {
+    let finalarray = _.isEqual(endArray, magicarray);
+
     if (finalarray === true) {
-        objStyle(bg[0], "none");
-        objStyle(bg[1], "block");
+        elementDisplayStatus(bg[0],"none");
+        elementDisplayStatus(bg[1],"block");
+
         setTimeout(function () {
-            objStyle(demo, "none");
-            objStyle(endcut, "block");
-        }, 3000)
+            elementDisplayStatus(demo,"none");
+            elementDisplayStatus(endcut,"block");
+        }, 1000)
     }
 
 }
+
+// add touch start listener
+
+canvas.addEventListener("touchstart", touchstart, false);
+canvas.addEventListener("touchmove", touchmove, false);
+canvas.addEventListener("touchend", touchend, false);
